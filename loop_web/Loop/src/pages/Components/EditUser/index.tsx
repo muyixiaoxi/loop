e355a9810efd22ecc2b8ddabfd62948cea41df0d
庞常顺 <1741349700@qq.com>
2025-04-05 09:09:40 +0800

    feat:封装路由hook

--- loop_web/Loop/src/pages/Components/EditUser/index.tsx
+++ loop_web/Loop/src/pages/Components/EditUser/index.tsx
@@ -1,29 +1,29 @@
-import './index.scss'
-import { useState, useEffect } from 'react'
+import "./index.scss";
+import { useState, useEffect } from "react";
 import { Form, Input, Select, InputNumber, Button, message } from "antd";
-import { idsearch } from '@/api/user'
-import { editUser } from '@/api/user';
+import { idsearch } from "@/api/user";
+import { editUser } from "@/api/user";
 import OSS from "ali-oss";
-import image1 from '../../../../public/avatar.jpg'
+import image1 from "../../../../public/avatar.jpg";
 
-const EditUser = () => { 
+const EditUser = () => {
   const [userData, setData] = useState({
     age: 0,
-    avatar: '',
+    avatar: "",
     gender: 0,
     is_friend: false,
-    nickname: '',
-    signature:''
+    nickname: "",
+    signature: "",
   });
   const [files, setFiles] = useState<FileItem[]>([]);
   const [isUploading, setIsUploading] = useState(false);
   const [previewUrl, setPreviewUrl] = useState("");
   const [nowdata, setNow] = useState({
     age: 0,
-    avatar: '',
+    avatar: "",
     gender: 0,
-    nickname: '',
-    signature:''
+    nickname: "",
+    signature: "",
   });
   const [isDataLoaded, setIsDataLoaded] = useState(false); // 控制数据是否加载完成
 
@@ -43,16 +43,17 @@ const EditUser = () => {
     secure: true, // 使用 HTTPS
   };
 
-  const fetchUser = async () => { 
-    const userid = JSON.parse(localStorage.getItem("userdata")).id;
-    const result = await idsearch(userid);
+  const fetchUser = async () => {
+    const userdata: string | any = localStorage.getItem("loop_userdata");
+    const userid = JSON.parse(userdata)?.id;
+    const result: any = await idsearch(userid);
     if (result && result.code === 1000) {
       setNow(result.data);
       setIsDataLoaded(true); // 标记数据已加载
     }
   };
 
-  useEffect(() => { 
+  useEffect(() => {
     fetchUser();
   }, []);
 
@@ -74,15 +75,15 @@ const EditUser = () => {
       // 更新 userData 的 avatar 属性
       setData((prevUserData) => ({
         ...prevUserData,
-        avatar: fileUrl
+        avatar: fileUrl,
       }));
 
       // 使用最新的 avatar 值调用 editUser
       const updatedUserData = {
         ...nowdata,
-        avatar: fileUrl
+        avatar: fileUrl,
       };
-      const res = await editUser(updatedUserData);
+      const res: any = await editUser(updatedUserData);
 
       if (res && res.code === 1000) {
         message.success("头像修改成功");
@@ -115,15 +116,15 @@ const EditUser = () => {
     input.click();
   };
 
-  const handleSubmit = async (values) => {
+  const handleSubmit = async (values: any) => {
     const updatedUserData = {
       ...nowdata,
       nickname: values.nickname,
       signature: values.signature,
       gender: values.gender,
-      age: values.age
+      age: values.age,
     };
-    const res = await editUser(updatedUserData);
+    const res: any = await editUser(updatedUserData);
     if (res && res.code === 1000) {
       message.success("个人信息修改成功");
       // 重新获取用户数据
@@ -135,7 +136,7 @@ const EditUser = () => {
 
   return (
     <div className="main1">
-      <div className='title'>修改个人信息</div>
+      <div className="title">修改个人信息</div>
       <div className="avatar1" onClick={triggerFileInput}>
         <img src={nowdata.avatar || image1} alt="avatar" />
         <div className="overlay">点击修改</div>
@@ -147,11 +148,11 @@ const EditUser = () => {
             nickname: nowdata.nickname,
             signature: nowdata.signature,
             gender: nowdata.gender,
-            age: nowdata.age
+            age: nowdata.age,
           }}
-          className='ddd'
-          size='large'
-          style={{ width: '80%', height: '50%',marginLeft:'16%' }}
+          className="ddd"
+          size="large"
+          style={{ width: "80%", height: "50%", marginLeft: "16%" }}
           onFinish={handleSubmit}
           labelAlign="left"
           labelCol={{ span: 3 }}
@@ -160,27 +161,27 @@ const EditUser = () => {
           <Form.Item
             name="nickname"
             label="昵称"
-            rules={[{ required: true, message: '请输入昵称' }]}
-            style={{ height: '30px' }}
-            className='formtitle'
+            rules={[{ required: true, message: "请输入昵称" }]}
+            style={{ height: "30px" }}
+            className="formtitle"
           >
             <Input placeholder="请输入昵称" />
           </Form.Item>
           <Form.Item
             name="signature"
             label="个性签名"
-            rules={[{ required: true, message: '请输入个性签名' }]}
-            style={{ height: '30px' }}
-            className='formtitle'
+            rules={[{ required: true, message: "请输入个性签名" }]}
+            style={{ height: "30px" }}
+            className="formtitle"
           >
             <Input placeholder="请输入个性签名" />
           </Form.Item>
           <Form.Item
             name="gender"
             label="性别"
-            rules={[{ required: true, message: '请选择性别' }]}
-            style={{ height: '30px' }}
-            className='formtitle'
+            rules={[{ required: true, message: "请选择性别" }]}
+            style={{ height: "30px" }}
+            className="formtitle"
           >
             <Select placeholder="请选择性别">
               <Select.Option value={0}>男性</Select.Option>
@@ -190,14 +191,18 @@ const EditUser = () => {
           <Form.Item
             name="age"
             label="年龄"
-            rules={[{ required: true, message: '请输入年龄' }]}
-            style={{ height: '30px' }}
-            className='formtitle'
+            rules={[{ required: true, message: "请输入年龄" }]}
+            style={{ height: "30px" }}
+            className="formtitle"
           >
             <InputNumber min={1} max={100} placeholder="请输入年龄" />
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" style={{width:'200px',height:'50px'}} >
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ width: "200px", height: "50px" }}
+            >
               保存
             </Button>
           </Form.Item>
