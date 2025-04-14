d3ee341fb552af7d9fcaf57b4f70ee19e43514e4
庞常顺 <1741349700@qq.com>
2025-04-14 20:12:59 +0800

    feat:整合代码

--- loop_web/Loop/vite.config.ts
+++ loop_web/Loop/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path' // 引入 path 模块
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // 引入 path 模块
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,9 +8,18 @@ export default defineConfig({
   resolve: {
     alias: {
       // 设置路径别名
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://47.93.85.12:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
